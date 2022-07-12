3c1fcd06554caf73db5d865271dc77f259b2570f
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-07-11 21:28:23 -0400

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Aster
+> Command line image colorizer utility.
+
+Aster is a simple command line tool to recolor images into a specific palette.  
+It is based on [felix-u's imgclr tool](https://github.com/felix-u/imgclr);
+this is somewhat a Go rewrite.
+
+Work in progress!
+
+# Features
+- [x] Recolor images
+## Formats
+- [x] JPEG
+- [ ] PNG
+- [ ] GIF
+
+# Install
+`go install github.com/TorchedSammy/Aster`
+
+# Usage
+`aster -i in.jpg -o out.jpg -p "#fff #000"` will recolor `in.jpg` to black
+and white, and write the result to `out.jpg`.
+
+# License
+MIT
---
+++ main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"os"
+	"strings"
+
+	// supported formats
+	"image/jpeg"
+
+	"github.com/spf13/pflag"
+)
+
+func main() {
+	inFlag := pflag.StringP("input", "i", "", "Path to input image")
+	outFlag := pflag.StringP("output", "o", "", "Path to output")
+	paletteFlag := pflag.StringP("palette", "p", "", "Palette")
+
+	pflag.Parse()
+	check(inFlag, "input")
+	check(outFlag, "output")
+	check(paletteFlag, "palette")
+
+	var palette color.Palette
+	for _, colorStr := range strings.Split(*paletteFlag, " ") {
+		col, err := strToColor(colorStr)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Invalid color", colorStr)
+		}
+		palette = append(palette, col)
+	}
+	
+	for _, p := range palette {
+		fmt.Println(p)
+	}
+	inFile, err := os.Open(*inFlag)
+	if err != nil {
+		perr("Could not open input file")
+	}
+	outFile, err := os.Create(*outFlag)
+	if err != nil {
+		perr("Could not create output file")
+	}
+
+	inImg, _, err := image.Decode(inFile)
+	if err != nil {
+		perr("Could not decode image:", err)
+	}
+
+	bounds := inImg.Bounds()
+	outImg := image.NewPaletted(bounds, palette)
+	draw.Draw(outImg, bounds, inImg, bounds.Min, draw.Src)
+	
+	jpeg.Encode(outFile, outImg, nil)
+}
+
+func check(flagVal *string, name string) {
+	if *flagVal == "" {
+		fmt.Fprintln(os.Stderr, "Missing flag", name)
+		pflag.Usage()
+		os.Exit(1)
+	}
+}
+
+func perr(str ...interface{}) {
+	fmt.Fprintln(os.Stderr, str...)
+	os.Exit(1)
+}
+
+func strToColor(str string) (color.Color, error) {
+	if str[0] == '#' {
+		// hexadecimal
+		normalStr, err := normalizeHex(str)
+		if err != nil {
+			return nil, err
+		}
+
+		b, err := hex.DecodeString(normalStr)
+		if err != nil {
+			return nil, err
+		}
+		
+		//                  r     g     b    a
+		return color.RGBA{b[0], b[1], b[2], 0xff}, nil
+	}
+
+	return nil, errors.New("Invalid format for color")
+}
+
+func normalizeHex(hx string) (string, error) {
+	switch len(hx) {
+		case 4: // #fff
+			longHex := ""
+	
+			for i, c := range hx {
+				if i == 0 { continue }
+				longHex = longHex + string(c) + string(c)
+			}
+
+			return longHex, nil
+		case 7: // #ffffff
+			return hx[1:], nil
+		default:
+			return "", errors.New("invalid string for hex")
+	}
+}
